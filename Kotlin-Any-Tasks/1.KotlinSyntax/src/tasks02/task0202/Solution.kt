39bf1179fe534e7443fac6b93674704aad260a46
RAMIL ARSAEV <gattoramm@gmail.com>
2022-02-03 21:57:58 +0300

    Create Solution.kt

---
+++ Kotlin-Any-Tasks/1.KotlinSyntax/src/tasks02/task0202/Solution.kt
@@ -0,0 +1,11 @@
+package tasks02.task0202
+
+/**
+ * Откуда берутся Person?
+ * */
+
+class Person(val name: String, val age: Int, val weight: Int, val money: Int)
+
+fun main() {
+    val person = Person("Bob", 25, 2, 1000)
+}
