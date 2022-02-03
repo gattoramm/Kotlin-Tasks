d26dc39374d5a99dc2031d9abfb41b011cdbb94d
RAMIL ARSAEV <gattoramm@gmail.com>
2022-02-03 21:57:48 +0300

    Create Solution.kt

---
+++ Kotlin-Any-Tasks/1.KotlinSyntax/src/task01/task0108/Solution.kt
@@ -0,0 +1,9 @@
+package task01.task0108
+
+/**
+ * Наш первый конвертер!
+ * */
+
+fun main() = print(convertCelsiusToFahrenheit(41))
+
+fun convertCelsiusToFahrenheit(celsius: Int): Double = 9 / 5.0 * celsius + 32
